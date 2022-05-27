0f0f4c195d0bd8f73e8c1dbb979a4209cd11499a
oviedoF <oviedofederico39@gmail.com>
2022-05-27 11:40:54 -0300

    tecnology card

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../../assets/tecnologies.json';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  
+  active: any = {};
+
+  setActive(e:Event){
+    const target = (<HTMLElement>e.target);
+    this.active = (data.filter(el => el.name === target.id))[0];
+
+    console.log(this.active);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.active = (data.filter(el => el.name === "html5"))[0];
   }
 
 }
--- src/app/main/about/tecnology-card/tecnology-card.component.ts
+++ src/app/main/about/tecnology-card/tecnology-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tecnology-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnology-card.component.scss']
 })
 export class TecnologyCardComponent implements OnInit {
+  @Input() habilityToCard: any;
 
   constructor() { }
 
